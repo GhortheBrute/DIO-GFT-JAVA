979405326fd2551e9079ffe785c358aafb551b5d
GhortheBrute <143045110+GhortheBrute@users.noreply.github.com>
2025-08-20 00:11:36 -0300

    Desafio POO Finalizado.

--- src/Desafio/Dominio/Dev.java
+++ src/Desafio/Dominio/Dev.java
@@ -1,7 +1,6 @@
 package Desafio.Dominio;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
     private String nome;
@@ -9,10 +8,64 @@ public class Dev {
     Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp){
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevs().add(this);
     }
 
     public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosInscritos.remove(conteudo.get());
+            this.conteudosConcluidos.add(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
+    public double calcularTotalExp(){
+        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
+        double soma = 0;
+        while (iterator.hasNext()){
+            double next = iterator.next().calcularExp();
+            soma += next;
+        }
+        return soma;
+        //return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularExp).sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
     }
 }
--- src/Desafio/Main.java
+++ src/Desafio/Main.java
@@ -1,7 +1,43 @@
 package Desafio;
 
+import Desafio.Dominio.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso("Curso Java", "Descrição Curso Java", 8);
 
+        Curso curso2 = new Curso("Curso JS", "Descrição Curso JS", 4);
+
+        Mentoria mentoria = new Mentoria("Mentoria de Java", "Descrição mentoria de java", LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJesus = new Dev();
+        devJesus.setNome("Jesus");
+        devJesus.inscreverBootcamp(bootcamp);
+        devJesus.progredir();
+        devJesus.progredir();
+        System.out.println("Conteúdos inscritos: " + devJesus.getNome() + ": " + devJesus.getConteudosInscritos());
+        System.out.println();
+        System.out.println("Conteúdos Concluídos: " + devJesus.getNome() + ": " + devJesus.getConteudosConcluidos());
+        System.out.println("EXP:" + devJesus.calcularTotalExp());
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("Conteúdos inscritos: " + devJoao.getNome() + ":" + devJoao.getConteudosInscritos());
+        System.out.println();
+        System.out.println("Conteúdos Concluídos: " + devJoao.getNome() + ":" + devJoao.getConteudosConcluidos());
+        System.out.println("EXP:" + devJoao.calcularTotalExp());
     }
 }
